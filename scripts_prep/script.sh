2ac75e4ad3cdcfefd6559b3a500a6884d5d2d989
Faidon Brotzakis <phaedon.brotzakis@gmail.com>
2024-10-26 01:09:44 +0300

    script.sh update

--- scripts_prep/script.sh
+++ scripts_prep/script.sh
@@ -28,19 +28,17 @@ plumed --no-mpi driver --plumed reconstruct.dat --mf_xtc cat_trjcat.xtc --timest
 
 python resample.py
 #Run the python script to make the fes plot
-num=1
-
+#num=1
 #For other proteins the entries CV1,CV2,CV3 etc need to follow the COLVAR columns like:
 #for i in $(echo CV1 CV2 CV3 etc);do
 
 ## For TDP-43 WtoA
-for i in $(echo Rg Rg1 Rg2 Rg3 Rg4 torsion1 torsion2 RMSD1 RMSD2 RMSD3);do
-python fes2.py --CV_col $num --CV_name $i
-num=$((num+1))
-echo $num
-done
+#for i in $(echo Rg Rg1 Rg2 Rg3 Rg4 torsion1 torsion2 RMSD1 RMSD2 RMSD3);do
+#python fes2.py --CV_col $num --CV_name $i
+#num=$((num+1))
+#echo $num
+#done
 
 python backmap.py $nrep
 sh pulchra.sh
 sh keepH.sh
-BEG
